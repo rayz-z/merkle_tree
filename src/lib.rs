331099dd76f76c18348fd830b08192ec096ec6b6
Raymond <raymond.z1859@gmail.com>
2025-07-20 18:47:38 -0400

    working out details for verify_node

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 use ark_ec;
 use ark_ff;
+use ark_sponge::{
+    CryptographicSponge,
+    poseidon::{self, PoseidonSponge},
+};
 use ark_std;
-
+use core::array;
+use core::cmp::PartialEq;
 
 /*
 -data structure defined tree
@@ -12,34 +17,77 @@ use ark_std;
 msgs are hashed and stored in leaf nodes
 */
 
-// node i 
+// node i
 // left child: 2i + 1
 // right child: 2i + 2
 // parent: floor((i - 2) / 2)
 struct MerkleTree {
     pub root: Node,
     storage: [Node; 31],
-    pub index: u8, // index of last node
+    pub index: usize, // index of last node
 }
 
+#[derive(Clone)]
+#[derive(PartialEq)]
 struct Node {
-    data: u8, 
+    data: u8,
+    index: usize,
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
 }
 
 impl MerkleTree {
     pub fn new(root: Node) -> Self {
-        Node
-        let storage: [Node; 31] = {}
-        MerkleTree { root, storage }
+        let storage: [Node; 31] = array::from_fn(|_| root.clone());
+
+        MerkleTree {
+            root,
+            storage,
+            index: 1,
+        }
     }
 
-    fn new_leaf()
+    fn new_leaf(&mut self, hash: u8) {
+        let node = Node {
+            data: hash,
+            index: self.index,
+            left: None,
+            right: None,
+        };
+
+        if self.index < 31 {
+            self.storage[self.index] = node;
+            self.index += 1;
+        } else {
+            println!("Max nodes reached");
+        }
+    }
 }
 
-// combine all nodes needed to prove node into an array of type proof? enum?
-fn prove_node() {}
+// take hash of node and hash each child with the other child up the tree and compare to root node
+fn verify_node(node: Node) -> bool {
+    let phash = PoseidonSponge::new(node);
+    let witnesses: Vec<u8> = vec![];
 
-// take hash of node and hash each child with the other child up the tree and compaire to root node
-fn verify_node() {}
+    let mut index = node.index.clone() as u8;
+
+    while index > 0 {
+        if index % 2 == 0 {
+            let sibling = index - 1;
+            witnesses.push(index);
+            witnesses.push(sibling);
+            index = (index - 2) / 2;
+        } else {
+            // check if right sibling exists
+            
+            index = (index - 2) / 2;
+        }
+    }
+
+    let mut proof_hash: u8 = witnesses[0];
+    for witness in 1..witnesses.len() {
+        // hash
+    }
+
+
+}
