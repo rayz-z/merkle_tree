8031dae2634323cd6b6a789342c805ef3e63bb2a
Raymond <raymond.z1859@gmail.com>
2025-07-29 20:52:38 -0400

    added display function on Node to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use core::array;
 use core::cmp::PartialEq;
-use sha2::{Sha256, Digest};
 use hex;
+use sha2::{Digest, Sha256};
+use std::error::Error;
 
 /*
 -data structure defined tree
@@ -18,8 +19,7 @@ pub struct MerkleTree {
     pub index: usize, // index of last node
 }
 
-#[derive(Clone)]
-#[derive(PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct Node {
     pub hash: String,
     pub index: usize,
@@ -27,8 +27,26 @@ pub struct Node {
     pub right: Option<Box<Node>>,
 }
 
+impl Node {
+    pub fn display(self: &Self) -> Result<(), Box<dyn Error>> {
+        println!("node: {}", self.hash);
+        if self.left == None && self.right == None {
+            println!("no left child or right child");
+        } else if self.left == None && !self.right.is_none() {
+            println!("right child: {}", self.right.as_ref().unwrap().hash);
+        } else if !self.left.is_none() && self.right == None {
+            println!("left child: {}", self.left.as_ref().unwrap().hash);
+        } else {
+            println!("left child: {}", self.left.as_ref().unwrap().hash);
+            println!("right child: {}", self.right.as_ref().unwrap().hash);
+        }
+        Ok(())
+    }
+}
+
 impl MerkleTree {
-    pub fn new(root: Node) -> Self {// why doesn't it error if I don't put in a node?
+    pub fn new(root: Node) -> Self {
+        // why doesn't it error if I don't put in a node?
         let storage: [Option<Node>; 31] = array::from_fn(|_| None);
 
         MerkleTree {
@@ -38,7 +56,8 @@ impl MerkleTree {
         }
     }
 
-    pub fn new_leaf(&mut self, msg: &str) { // why do we need to add pub?
+    pub fn new_leaf(&mut self, msg: &str) {
+        // why do we need to add pub?
         let msg = String::from(msg);
         let hash = Sha256::digest(msg.as_bytes());
         let hash = hex::encode(hash);
@@ -59,26 +78,26 @@ impl MerkleTree {
 
         // need to add conditions to update the parent and ancestor nodes
         let indexer = self.index;
-        while indexer > 0 {
-            
-        }
+        while indexer > 0 {}
     }
 
     pub fn search_for_node_at_index(self: &Self, index: usize) -> Option<Node> {
-        if index < 0 || index > 31 || index > self.index {
+        if index > 31 || index > self.index {
             panic!("index out of bounds");
         } else {
             self.storage[index].clone()
         }
     }
 
-    pub fn delete_node() {}
+    pub fn delete_node() {} // take an index and shift all others behind it up -> an array shift but also reassign parents/hashes
 
-    pub fn display() {}
+    pub fn display(self: &Self) {
+
+    }
 }
 
 // take hash of node and hash each child with the other child up the tree and compare to root node
-fn verify_node(node: Node, tree: MerkleTree) -> bool {
+pub fn verify_node(node: Node, tree: MerkleTree) -> bool {
     let mut path_to_node: Vec<usize> = vec![];
 
     let mut index = node.index.clone();
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use sha2::{self, Digest, Sha256};
 use hex;
 
 fn main() {
-    println!("Hello, world!");
-
 
     let hash = Sha256::digest(b"hello");
     let hash = hex::encode(hash);
-    let node = Node{ hash, index: 0, left: None, right: None }; // what if index not 0?
+    let node1 = Node{ hash, index: 0, left: None, right: None }; // what if index not 0?
+    let node2 = Node{ hash: String::from("hey"), index: 1, left: None, right: None };
+    let node3 = Node{ hash: String::from("hi"), index: 2, left: Some(Box::new(node1)), right: Some(Box::new(node2)) };
 
-    let tree = MerkleTree::new(node);
+    let _ = node3.display();
+    // let tree = MerkleTree::new(node);
 }
